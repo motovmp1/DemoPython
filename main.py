1bc2ac420ac3dc0fa54138cfa6a2b31da6f5e85f
vinicius pinho <viniciuspinho@viniciuss-MacBook-Pro.local>
2023-03-02 19:57:44 +0000

    Second commit and new file

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+display_message = [
+    'Seja feliz :)',
+    'Fque tranquilo, tudo vai acabar bem!'
+
+]
+
+while True:
+    resposta = input('Deseja receber um concelho?  S/N : ')
+    if (resposta == 'S' or resposta == 's'):
+        mensagem = random.choice(display_message)
+        print(mensagem)
+        print()
+
